c34bc4afe77b43d33e602828546ba47788f727a5
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-05-14 09:11:17 +0300

    Changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+#!/usr/bin/env python3
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted_word and candidate.lower() != self.word:
+                matches.append(candidate)
+
+        return matches
+
+    
